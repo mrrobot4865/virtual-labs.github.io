f4d1a756b718648e62e73f7cc3a25a9fc979c448
Sai Vishnu <mrrobot4865@gmail.com>
2024-06-29 16:10:33 +0530

    Table Page Updated

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+document.getElementById("button-1").addEventListener("click", function() {
+    window.location.href = "./pages/table/table.html"; // Replace "another_page.html" with the URL of the page you want to navigate to
+  });
+
+  document.getElementById("button-2").addEventListener("click", function() {
+    window.location.href = "./pages/table/table.html"; // Replace "another_page.html" with the URL of the page you want to navigate to
+  });
